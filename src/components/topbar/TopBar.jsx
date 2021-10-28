2efd02d5ae249c6d2c2a76bc8e560a27cbe07183
kendrickPC <kennethpchang@gmail.com>
2021-10-28 16:18:37 -0700

    login and register components and react router dom links working for basic use

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
+  const user = false;
   return (
     <div className="top">
 
@@ -12,15 +14,31 @@ export default function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem"><Link to="/" className="link">HOME</Link></li>
+          <li className="topListItem"><Link to="/" className="link">ABOUT</Link></li>
+          <li className="topListItem"><Link to="/" className="link">CONTACT</Link></li>
+          <li className="topListItem"><Link to="/write" className="link">WRITE</Link></li>
+          <li className="topListItem">
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img className="topImage" src="https://i.guim.co.uk/img/media/03b248a38342922414013d948ea68909b83daa14/0_100_3000_1800/master/3000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c5b9bfe743f52324231b0f976d9c3a6f" alt="Elmo in a suit" />
+        {
+          user ? (
+            <img className="topImage" src="https://i.guim.co.uk/img/media/03b248a38342922414013d948ea68909b83daa14/0_100_3000_1800/master/3000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c5b9bfe743f52324231b0f976d9c3a6f" alt="Elmo in a suit" />
+          ) : (
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">LOGIN</Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/register">REGISTER</Link>
+              </li>
+            </ul>
+          )
+        }
+        
         <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
